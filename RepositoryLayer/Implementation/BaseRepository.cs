eaba11a248ce1ffc0084c3ae75236ae141b2ca85
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-03 23:33:17 +0530

    [Kunal] ADD: DBContext for communication with database

---
+++ RepositoryLayer/Implementation/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace RepositoryLayer.Implementation
+{
+    class BaseRepository : IBaseRepository
+    {
+        public string ConnectionString { get; set; }
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
