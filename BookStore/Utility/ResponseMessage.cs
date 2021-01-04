a9c2efc533ae3ca94da36e59eff82460d17fbdda
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 19:05:13 +0530

    [Kunal] ADD: Seperate response message file for http response

---
+++ BookStore/Utility/ResponseMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class ResponseMessage
+    {
+        public const string LOGIN_SUCCESS = "Login successful";
+        public const string USER_ADDED = "User created successfully";
+    }
+}
