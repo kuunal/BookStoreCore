b3b1cb53fbd0944baea458f6b32d68d4825ad6cd
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 23:39:29 +0530

    [Kunal] ADD: Method for inserting book in repository

---
+++ RepositoryLayer/Implementation/BooksRepository.cs
@@ -0,0 +1,35 @@
+﻿using ModelLayer.BookDto;
+using RepositoryLayer.Interface;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Implementation
+{
+    public class BooksRepository : IBooksRepository
+    {
+        private readonly SqlConnection _conn;
+
+        public BooksRepository(IDBContext dBContext)
+        {
+            _conn = dBContext.GetConnection();
+        }
+
+        public async Task<int> Insert(BookRequestDto requestDto)
+        {
+            SqlCommand command = new SqlCommand("sp_books_insert", _conn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@description", requestDto.Description);
+            command.Parameters.AddWithValue("@title", requestDto.Title);
+            command.Parameters.AddWithValue("@author", requestDto.Author);
+            command.Parameters.AddWithValue("@price", requestDto.Price);
+            command.Parameters.AddWithValue("@quantity", requestDto.Quantity);
+            await _conn.OpenAsync();
+            int id = Convert.ToInt32(await command.ExecuteScalarAsync());
+            await _conn.CloseAsync();
+            return id;
+        }
+    }
+}
---
+++ RepositoryLayer/Interface/IBooksRepository.cs
@@ -0,0 +1,13 @@
+﻿using ModelLayer.BookDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IBooksRepository
+    {
+        Task<int> Insert(BookRequestDto requestDto);
+    }
+}
