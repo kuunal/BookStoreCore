1f061366f0d164bcdcd5148a788997fb16c61008
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 23:20:22 +0530

    [Kunal] ADD: Dto for book CRUD

---
+++ ModelLayer/BookDto/BookResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModelLayer.BookDto
+{
+    public class BookResponseDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        [Url]
+        public string Image { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+    }
+}
