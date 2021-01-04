154a13026c26f064313e4329ca78bc0813282cc6
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 19:07:21 +0530

    [Kunal] ADD: Automapper for request to response dto

---
+++ BookStore/Utility/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ModelLayer;
+using ModelLayer.UserDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Utility
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserRequestDto, UserResponseDto>();
+        }
+    }
+}
