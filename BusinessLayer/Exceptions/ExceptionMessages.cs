f396ec903cfc662184f278fee4e64667ae55b628
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 18:46:29 +0530

    [Kunal] ADD: Seperate file for exception messages

---
+++ BusinessLayer/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Exceptions
+{
+    public class ExceptionMessages
+    {
+        public const string ACCOUNT_ALREADY_EXISTS = "Account with email already exist!";
+    }
+}
