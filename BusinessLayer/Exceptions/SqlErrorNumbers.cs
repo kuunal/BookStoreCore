e5de4a50a458445f85dc1f55f5c3e008c2f39445
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-04 19:06:51 +0530

    [Kunal] ADD: Distinguishing exception type of SQLException

---
+++ BusinessLayer/Exceptions/SqlErrorNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Implementation
+{
+    public class SqlErrorNumbers
+    {
+        public const int DuplicateKey = 2627;
+    }
+}
