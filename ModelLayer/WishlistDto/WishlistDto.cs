323108eda39e429fce8f39b77fe12c394bd77f51
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-05 17:18:31 +0530

    [Kunal] ADD: Dto for wishlist

---
+++ ModelLayer/WishlistDto/WishlistDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.WishlistDto
+{
+    public class Wishlist
+    {
+        public int userId { get; set; }
+        public int bookId { get; set; }
+    }
+}
