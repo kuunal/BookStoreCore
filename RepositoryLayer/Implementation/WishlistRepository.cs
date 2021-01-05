8823a52bd97ac7a39fa85846e1be567d6b0c72e8
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-05 20:08:56 +0530

    [Kunal] ADD: Insert method in repository

---
+++ RepositoryLayer/Implementation/WishlistRepository.cs
@@ -0,0 +1,34 @@
+﻿using ModelLayer.WishlistDto;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Implementation
+{
+    public class WishlistRepository : IWishlistRepository
+    {
+        private readonly SqlConnection _conn;
+
+        public WishlistRepository(IDBContext dBContext)
+        {
+            _conn = dBContext.GetConnection();
+        }
+
+        public async Task<int> Insert(WishlistDto wishlist)
+        {
+            SqlCommand command = new SqlCommand("sp_wishlist_insert", _conn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            command.Parameters.AddWithValue("@userId", wishlist.userId);
+            command.Parameters.AddWithValue("@bookId", wishlist.bookId);
+            await _conn.OpenAsync();
+            int isInserted = await command.ExecuteNonQueryAsync();
+            await _conn.CloseAsync();
+            return isInserted;
+        }
+    }
+}
---
+++ RepositoryLayer/Interface/IWishlistRepository.cs
@@ -0,0 +1,13 @@
+﻿using ModelLayer.WishlistDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Interface
+{
+    public interface IWishlistRepository
+    {
+        Task<int> Insert(WishlistDto wishlist);
+    }
+}
