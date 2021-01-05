46129d85a0cc6e0c3728dc058d0b9841850c2881
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-06 00:30:31 +0530

    [Kunal] ADD: Dto for car

---
+++ ModelLayer/CartDto/CartResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.CartDto
+{
+    public class CartResponseDto
+    {
+        public int userId { get; set; }
+        public int bookId { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+
+    }
+}
--- ModelLayer/WishlistDto/WishlistDto.cs
+++ ModelLayer/WishlistDto/WishlistDto.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ModelLayer.WishlistDto
 {
     public class WishlistDto
     {
+        [Range(1, int.MaxValue)]
         public int UserId { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int BookId { get; set; }
     }
 }
