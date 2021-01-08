f5b86bb9f8bdfb892d10693d8b3fa9b0eb4ce0b6
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-09 01:50:55 +0530

    [Kunal] ADD: Sub header

--- Frontend/src/app/components/dashboard/dashboard.component.ts
+++ Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  total: number = 54;
+  isDropDownOpen: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
