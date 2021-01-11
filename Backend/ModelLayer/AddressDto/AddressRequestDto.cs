1685da51a5f6baf78cced8ccd6e0df1388c148d7
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-11 18:57:48 +0530

    [Kunal] ADD: ADdress dto

---
+++ Backend/ModelLayer/AddressDto/AddressRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.AddressDto
+{
+    public class AddressRequestDto
+    {
+        public string house { get; set; }
+        public string street { get; set; }
+        public string locality { get; set; }
+        public string city { get; set; }
+        public int pincode { get; set; }
+    }
+}
---
+++ Backend/ModelLayer/AddressDto/AddressResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.AddressDto
+{
+    public class AddressResponseDto
+    {
+        public int id { get; set; }
+        public string house { get; set; }
+        public string street { get; set; }
+        public string locality { get; set; }
+        public string city { get; set; }
+        public int pincode { get; set; }
+    }
+}
