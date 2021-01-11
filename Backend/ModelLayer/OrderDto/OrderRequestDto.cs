40fdd03ef90316861c0049fb8a830f7fb52558c0
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-11 19:20:41 +0530

    [Kunal] ADD: Dto for orders

---
+++ Backend/ModelLayer/OrderDto/OrderRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ModelLayer.OrderDto
+{
+    public class OrderRequestDto
+    {
+        [Range(1, int.MaxValue)]
+        public int bookId { get; set; }
+        
+        [Range(1, int.MaxValue)]
+        public int userId { get; set; }
+        
+        [Range(1, int.MaxValue)]
+        public int quantity { get; set; }
+        public string orderId { get; set; }
+    }
+}
---
+++ Backend/ModelLayer/OrderDto/OrderResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.OrderDto
+{
+    public class OrderResponseDto
+    {
+        public int id { get; set; }
+        public int bookId { get; set; }
+        public int userId { get; set; }
+        public int quantity { get; set; }
+        public DateTime orderedDate { get; set; }
+        public string orderId { get; set; }
+    }
+}
