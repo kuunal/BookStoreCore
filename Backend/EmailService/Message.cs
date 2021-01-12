cae269bdf1d87ac8e11bbeb853b64c5d0a4c47a5
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-12 12:57:00 +0530

    [Kunal] ADD: Email service

---
+++ Backend/EmailService/Message.cs
@@ -0,0 +1,21 @@
+﻿using MimeKit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmailService
+{
+    public class Message
+    {
+        public List<string> To;
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<string>();
+            To.AddRange(to);
+            Subject = subject;
+            Content = content; 
+        }
+    }
+}
