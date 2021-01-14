2dd14f5e0aabf75bc152e27c6f4aabf5756c8d6e
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-14 21:03:45 +0530

    [Kunal] ADD: Attribute for caching

--- Backend/Caching/CachedAttribute.cs
+++ Backend/Caching/CachedAttribute.cs
@@ -35,82 +35,22 @@ namespace Caching
 
             if (!string.IsNullOrEmpty(cachedResponse) && context.HttpContext.Request.Method == "GET")
             {
-                var contentResult = new ContentResult
+                    var contentResult = new ContentResult
                 {
-                    Content =  
-                     JsonConvert.SerializeObject(new {
-                        StatusCode= 200,
-                        Message = "Success",
-                        Data=cachedResponse,
-                    }),
+                    Content =
+                     JsonConvert.SerializeObject(new Response<CartDetailedResponseDto>
+                     {
+                         StatusCode = 200,
+                         Message = "Success",
+                         Data = JsonConvert.DeserializeObject<CartDetailedResponseDto>(cachedResponse),
+                     }),
                     ContentType = "application/json",
                     StatusCode = 200
                 };
                 context.Result = contentResult;
                 return;
             }
-
-
             var executedContextResult = await next();
-
-            if (executedContextResult.Result is OkObjectResult okObjectResult)
-            {
-                string responseData = JsonConvert.SerializeObject(okObjectResult.Value);
-                string[] pathArray = context.HttpContext.Request.Path.Value.Split("/");
-                int? pathParam = null;
-                string cachedData = await cachedService.GetCachedResponseAsync(key);
-
-                List<CartResponseDto> currentUserData = null;
-                if (cachedData != null)
-                {
-                    currentUserData = JsonConvert.DeserializeObject<List<CartResponseDto>>(cachedData);
-                }
-                if (pathArray[pathArray.Length - 1].All(char.IsDigit))
-                {
-                    pathParam = Convert.ToInt32(pathArray[pathArray.Length - 1]);
-                }
-
-                switch (context.HttpContext.Request.Method)
-                {
-                    case "GET":
-                        var responseDataList = JsonConvert.DeserializeObject<Response<List<CartResponseDto>>>(responseData).Data;
-                        await cachedService.CacheResponseAsync(key, responseDataList, TimeSpan.FromSeconds(_timeToLiveSeconds));
-                        break;
-                    case "POST":
-                        if (cachedData == null)
-                        {
-                            break;
-                        }
-                        int? bookId = pathParam;
-                        if (bookId == null)
-                        {
-                            var deserializedResponse = JsonConvert.DeserializeObject<Response<CartResponseDto>>(responseData).Data;
-                            currentUserData.Add(deserializedResponse);
-                        }
-                        else
-                        {
-                            var deserializedResponse = JsonConvert.DeserializeObject<Response<CartResponseDto>>(responseData).Data;
-                            currentUserData.Where(cart => cart.Book.Id == bookId).Select(cart => cart = deserializedResponse);
-                        }
-                        await cachedService.CacheResponseAsync(key, currentUserData, TimeSpan.FromSeconds(600));
-                        break;
-                    case "DELETE":
-                        bookId = pathParam;
-                        if (cachedData == null)
-                        {
-                            break;
-                        }
-                        if (currentUserData.Remove(currentUserData.FirstOrDefault(cart => cart.Book.Id == bookId)))
-                        {
-                            await cachedService.CacheResponseAsync(key, currentUserData, TimeSpan.FromSeconds(600));
-                        }
-                        break;
-                    default:
-                        throw new Exception("Something went wrong");
-                }
-            }
-
-
         }
     }
 }
