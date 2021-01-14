af1140ab772552fe3c5939abce968008eb3865aa
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-14 21:22:55 +0530

    [Kunal] ADD: Repository for caching cart items

---
+++ Backend/Caching/CacheRepository.cs
@@ -0,0 +1,63 @@
+﻿using Caching;
+using ModelLayer.CartDto;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caching
+{
+    public class CacheRepository : ICacheRepository
+    {
+        private readonly IResponseCacheService _service;
+
+        public CacheRepository(IResponseCacheService service)
+        {
+            _service = service;
+        }
+        public async Task AddAsync(string key, CartResponseDto cart)
+        {
+            CartDetailedResponseDto cartDetailed = JsonConvert
+                                        .DeserializeObject<CartDetailedResponseDto>(await _service
+                                        .GetCachedResponseAsync(key)
+                                        );
+            if (cartDetailed == null)
+                return;
+            cartDetailed.cartItems.Add(cart);
+            await _service.CacheResponseAsync(key, cartDetailed, TimeSpan.FromSeconds(600));
+        }
+
+        public async Task DeleteAsync(string key, int bookId)
+        {
+            CartDetailedResponseDto cartDetailed = JsonConvert
+                                                    .DeserializeObject<CartDetailedResponseDto>(await _service
+                                                    .GetCachedResponseAsync(key)
+                                                    );
+            if (cartDetailed == null)
+                return;
+            cartDetailed.cartItems.Remove(cartDetailed.cartItems.FirstOrDefault(item=>item.Book.Id == bookId));
+            await _service.CacheResponseAsync(key, cartDetailed, TimeSpan.FromSeconds(600));        
+        }
+
+        public async Task Get(string key, CartDetailedResponseDto response)
+        {
+            if (response == null)
+                return;
+            await _service.CacheResponseAsync(key, response, TimeSpan.FromSeconds(599));
+        }
+
+        public async Task UpdateAsync(string key, CartResponseDto requestDto, int bookId)
+        {
+            CartDetailedResponseDto cartDetailed = JsonConvert
+                                        .DeserializeObject<CartDetailedResponseDto>(await _service
+                                        .GetCachedResponseAsync(key)
+                                        );
+            if (cartDetailed == null)
+                return;
+            cartDetailed.cartItems.Where(item=>item.Book.Id == bookId).Select(item=>item.ItemQuantity = requestDto.ItemQuantity);
+            await _service.CacheResponseAsync(key, cartDetailed, TimeSpan.FromSeconds(600));
+        }
+    }
+}
---
+++ Backend/Caching/ICacheRepository.cs
@@ -0,0 +1,18 @@
+﻿using ModelLayer.CartDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Caching
+{
+    public interface ICacheRepository
+    {
+        Task Get(string key, CartDetailedResponseDto response);
+        Task UpdateAsync(string key, CartResponseDto requestDto, int bookId);
+        Task AddAsync(string key, CartResponseDto cart);
+        Task DeleteAsync(string key, int bookId);
+
+    }
+}
