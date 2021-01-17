18716ff0b4850486dd815920d42e751d5f6c6d9f
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-18 01:38:01 +0530

    [Kunal] ADD: Get address in services

---
+++ Backend/BusinessLayer/Implementation/AddressService.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Interface;
+using ModelLayer.AddressDto;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Implementation
+{
+    public class AddressService : IAddressService
+    {
+        private readonly IAddressRepository _repository;
+
+        public AddressService(IAddressRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<AddressResponseDto> GetAddress(int userId)
+        {
+            return await _repository.GetAddress(userId);
+        }
+    }
+}
---
+++ Backend/BusinessLayer/Interface/IAddressService.cs
@@ -0,0 +1,13 @@
+﻿using ModelLayer.AddressDto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IAddressService
+    {
+        Task<AddressResponseDto> GetAddress(int userId);
+    }
+}
