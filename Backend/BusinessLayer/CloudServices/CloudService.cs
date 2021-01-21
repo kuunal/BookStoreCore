42ff0187a95dfb01d555c553e06175347fac2798
kuunal <kunaldeshmukh2503@gmail.com>
2021-01-21 05:40:51 +0530

    [Kunal] ADD: Cloud service fo imgae upload

---
+++ Backend/BusinessLayer/CloudServices/CloudService.cs
@@ -0,0 +1,41 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.CloudServices
+{
+    public class CloudService : ICloudService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudService(CloudConfiguration config)
+        {
+            Account account = new Account(config.Name
+                                          , config.ApiKey
+                                          , config.Secret);
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<string> UpdloadToCloud(IFormFile image, string email)
+        {
+            var uploadResult = new ImageUploadResult();
+            if (image.Length > 0)
+            {
+                using (var stream = image.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(email + DateTime.Now.ToString(), stream)
+                    };
+
+                    uploadResult = await _cloudinary.UploadAsync(uploadParams);
+                }
+            }
+            return uploadResult.Url.ToString();
+        }
+    }
+}
---
+++ Backend/BusinessLayer/CloudServices/ICloudService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.CloudServices
+{
+    public interface ICloudService
+    {
+        Task<string> UpdloadToCloud(IFormFile image, string email);
+    }
+}
